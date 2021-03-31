75f1122a1aa97e4736eb9ee03cf0d7885a3ef6d3
Tomasz Piowczyk <tomasz.piowczyk@gmail.com>
2021-03-31 13:30:25 +0200

    Add SearchBox control

--- RPGDataEditor/RPGDataEditor.Wpf/Modules/Npc/Views/NpcTab.xaml.cs
+++ RPGDataEditor/RPGDataEditor.Wpf/Modules/Npc/Views/NpcTab.xaml.cs
@@ -1,8 +1,6 @@
 ﻿using RPGDataEditor.Core.Models;
 using System;
-using System.ComponentModel;
 using System.Windows.Controls;
-using System.Windows.Data;
 
 namespace RPGDataEditor.Wpf.Npc.Views
 {
@@ -11,35 +9,21 @@ namespace RPGDataEditor.Wpf.Npc.Views
         public NpcTab()
         {
             InitializeComponent();
-            DependencyPropertyDescriptor dpd = DependencyPropertyDescriptor.FromProperty(ItemsControl.ItemsSourceProperty, typeof(ListView));
-            dpd.AddValueChanged(NpcsList, OnNpcListBindingChanged);
             NpcsList.Focus();
         }
 
-        private void OnNpcListBindingChanged(object sender, EventArgs e)
+        private bool NpcFilter(object item, string searchText)
         {
-            ICollectionView view = CollectionViewSource.GetDefaultView(NpcsList.ItemsSource);
-            if (view != null)
-            {
-                view.Filter = NpcFilter;
-            }
-        }
-
-        private bool NpcFilter(object item)
-        {
-            if (string.IsNullOrEmpty(SearchTextBox.Text))
+            if (string.IsNullOrEmpty(searchText))
             {
                 return true;
             }
-            string name = SearchTextBox.Text;
             SimpleIdentifiableData npc = (SimpleIdentifiableData)item;
-            if (int.TryParse(name, out int id))
+            if (int.TryParse(searchText, out int id))
             {
                 return npc.Id == id;
             }
-            return npc.Name.Contains(name, StringComparison.OrdinalIgnoreCase);
+            return npc.Name.Contains(searchText, StringComparison.OrdinalIgnoreCase);
         }
-
-        private void SearchTextBox_TextChanged(object sender, TextChangedEventArgs e) => CollectionViewSource.GetDefaultView(NpcsList.ItemsSource).Refresh();
     }
 }
